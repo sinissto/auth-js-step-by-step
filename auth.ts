e0c7fa7db1d153dae21c9bb9ca7cf51501ad5600
Sinisa <sinisha.stojanovic@gmail.com>
2025-09-29 20:43:43 +0200

    auth.ts and route.ts configured (  see docs for more info )

--- auth.ts
+++ auth.ts
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth";
+import GitHub from "@auth/core/providers/github";
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
-  providers: [],
+  providers: [GitHub],
 });
