27a98dc41321fbf298b57c633f3c765eaaa4435c
Sinisa <sinisha.stojanovic@gmail.com>
2025-09-29 23:06:04 +0200

    Login with Next-Auth "signIn()" witch has been exported from ./auth.ts

--- actions/login.ts
+++ actions/login.ts
@@ -2,11 +2,33 @@
 
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "@auth/core/errors";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
 
   if (!validatedFields.success) return { error: "Invalid fields" };
 
-  return { success: "Email sent" };
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid email or password" };
+        default:
+          return { error: "Something went wrong, please try again later." };
+      }
+    }
+
+    throw error;
+  }
 };
