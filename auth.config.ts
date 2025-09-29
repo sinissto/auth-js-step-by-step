9707f4d4ab910ba8284de439be4ae62615465c94
Sinisa <sinisha.stojanovic@gmail.com>
2025-09-29 22:56:52 +0200

    Implemented Next-Auth Credentials as login provider, and exported "signIn" and "signOut" fns in ./auth.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,31 @@
-import GitHub from "next-auth/providers/github"
+import Credential from "next-auth/providers/credentials"
 import type { NextAuthConfig } from "next-auth"
+import {LoginSchema} from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+import bcrypt from "bcryptjs";
+
 
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GitHub],
+  providers: [Credential({
+   authorize: async (credentials) => {
+      const validatedFields = LoginSchema.safeParse(credentials)
+
+      if (validatedFields.success) {
+        const {email, password} = validatedFields.data
+
+        const user = await getUserByEmail(email)
+
+        if(!user || !user.password) return null
+
+        const passwordsMatch = await bcrypt.compare(password, user.password)
+
+        if(passwordsMatch) {
+          return user
+        }
+      }
+
+      return null
+    }
+  })],
 } satisfies NextAuthConfig
