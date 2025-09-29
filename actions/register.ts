cdc34f6793c1bbf9faa84bcdba6da7e69e3ad4b5
Sinisa <sinisha.stojanovic@gmail.com>
2025-09-29 19:10:25 +0200

    Created data util to fetch user data from db by "email" and "id", and implemented in "register" Server Action

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,7 @@ import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
 import bcrypt from "bcryptjs";
 import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -14,9 +15,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
   const hashedPassword = await bcrypt.hash(password, 12);
 
-  const existingUser = await db.user.findUnique({
-    where: { email },
-  });
+  const existingUser = await getUserByEmail(email);
 
   if (existingUser) return { error: "Email already exists" };
 
