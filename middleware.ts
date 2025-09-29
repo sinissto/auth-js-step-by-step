ea72c42ffcf8a4e1b6b4ee135c55668de61b627e
Sinisa <sinisha.stojanovic@gmail.com>
2025-09-29 22:42:10 +0200

    Middleware updated to handle different routes depending of if the user is logged in or logged out

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,34 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
+import {
+  apiAuthPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+} from "@/routes";
 
 const { auth } = NextAuth({ ...authConfig });
 
 export default auth((req) => {
-  console.log("Middleware auth request:", req.nextUrl.pathname);
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isAuthRoute) {
+    if (isLoggedIn)
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL(`/auth/login`, nextUrl));
+  }
+
+  return null;
 });
 
 export const config = {
--- routes.ts
+++ routes.ts
@@ -6,21 +6,21 @@
 export const publicRoutes = ["/"];
 
 /**
- * An array of routes that are used for authentication
- * These routes will redirect logged users to /settings
+ * An array of routes that are used for authentication.
+ * These routes will redirect logged users to /settings.
  * @type {string[]}
  */
 export const authRoutes = ["/auth/login", "/auth/register"];
 
 /**
- * The prefix for API authentication routes
- * Routes that start with this prefix are used for API auth purposes
+ * The prefix for API authentication routes.
+ * Routes that start with this prefix are used for API auth purposes.
  * @type {string}
  */
 export const apiAuthPrefix = "/api/auth";
 
 /**
- * The default redirect path after logging in
+ * The default redirect path after logging in.
  * @type {string}
  */
 export const DEFAULT_LOGIN_REDIRECT = "/settings";
