fd4f3db68b89ba26037274d07832922f44f1252d
Sinisa <sinisha.stojanovic@gmail.com>
2025-09-29 14:47:06 +0200

    RegisterSchema created

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,11 @@ export const LoginSchema = z.object({
   email: z.email({ message: "Email is required" }),
   password: z.string().min(1, { message: "Password is required" }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.email({ message: "Email is required" }),
+  password: z
+    .string()
+    .min(6, { message: "Password is required, Minimum 6 characters!" }),
+  name: z.string().min(1, { message: "Name is required" }),
+});
