ca8aa5fc3e1cbab8fcf3ea79ccdd25d0315bd733
Sinisa <sinisha.stojanovic@gmail.com>
2025-09-30 17:21:05 +0200

    Google and Github added as login option to corresponding buttons in Social.tsx ( signIn() has been imported to manage login on frontend, it could be also done with server actions )

--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -1,15 +1,24 @@
+"use client";
+
+import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, { redirectTo: DEFAULT_LOGIN_REDIRECT }).then((result) => {
+      console.log(result);
+    });
+  };
   return (
     <div className={"flex items-center w-full gap-x-2"}>
       <Button
         size={"lg"}
         variant={"outline"}
         className={"flex-1"}
-        onClick={() => {}}
+        onClick={() => onClick("google")}
       >
         <FcGoogle className={"h-5 w-5"} />
       </Button>
@@ -17,7 +26,7 @@ const Social = () => {
         size={"lg"}
         variant={"outline"}
         className={"flex-1"}
-        onClick={() => {}}
+        onClick={() => onClick("github")}
       >
         <FaGithub className={"h-5 w-5"} />
       </Button>
