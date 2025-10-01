b24d992ab03a104b65528344bccf09a6e4477259
Sinisa <sinisha.stojanovic@gmail.com>
2025-10-01 16:35:49 +0200

    Checking if user has verified his email before login, and if not generate verification token again to send it to user's email

--- actions/login.ts
+++ actions/login.ts
@@ -5,6 +5,8 @@ import { LoginSchema } from "@/schemas";
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "@auth/core/errors";
+import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/data/tokens";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -12,6 +14,22 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   if (!validatedFields.success) return { error: "Invalid fields" };
 
   const { email, password } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser || !existingUser.email || !existingUser.password)
+    return { error: "Email does not exist" };
+
+  if (!existingUser.emailVerified) {
+    const verificationToken = await generateVerificationToken(
+      existingUser.email
+    );
+
+    return {
+      success:
+        "Conformation email sent! Please check your inbox to verify your email address.",
+    };
+  }
 
   try {
     await signIn("credentials", {
