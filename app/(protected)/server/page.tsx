27fcc4a65066d6ce07b056e17e84a6f25739e82b
Sinisa <sinisha.stojanovic@gmail.com>
2025-10-02 19:41:13 +0200

    Server component

---
+++ app/(protected)/server/page.tsx
@@ -0,0 +1,9 @@
+import { currentUser } from "@/lib/auth";
+import UserInfo from "@/components/UserInfo";
+
+const ServerPage = async () => {
+  const user = await currentUser();
+  return <UserInfo label={"Server component"} user={user} />;
+};
+
+export default ServerPage;
