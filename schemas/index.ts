602cf8fa391d0e3dff4cb0057540cec34b6d4b09
Sinisa <sinisha.stojanovic@gmail.com>
2025-10-03 10:32:20 +0200

    SettingsSchema fully updated to all relevant fields of settings page

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,5 @@
 import * as z from "zod";
+import { UserRole } from "@/lib/generated/prisma";
 
 export const LoginSchema = z.object({
   email: z.email({ message: "Email is required" }),
@@ -26,6 +27,34 @@ export const NewPasswordResetSchema = z.object({
   }),
 });
 
-export const SettingsSchema = z.object({
-  name: z.string().min(1, { message: "Name is required" }),
-});
+export const SettingsSchema = z
+  .object({
+    name: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.email()),
+    password: z.optional(z.string().min(6)),
+    newPassword: z.optional(z.string().min(6)),
+  })
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) return false;
+
+      return true;
+    },
+    {
+      message: "New password is required",
+      path: ["newPassword"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.newPassword && !data.password) return false;
+
+      return true;
+    },
+    {
+      message: "Password is required",
+      path: ["password"],
+    }
+  );
