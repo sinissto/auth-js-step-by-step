c7e6c1a2c3d00a7641339760f02dc24d5da0c1cc
Sinisa <sinisha.stojanovic@gmail.com>
2025-10-03 09:10:33 +0200

    Update users's data server action

---
+++ actions/settings.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import * as z from "zod";
+import { SettingsSchema } from "@/schemas";
+import { getUserById } from "@/data/user";
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
+
+export const settings = async (values: z.infer<typeof SettingsSchema>) => {
+  const user = await currentUser();
+
+  if (!user) return { error: "Unauthorized" };
+
+  let dbUser;
+
+  if (user.id) dbUser = await getUserById(user.id);
+
+  if (!dbUser) return { error: "Unauthorized" };
+
+  await db.user.update({
+    where: { id: dbUser.id },
+    data: { ...values },
+  });
+
+  return { success: "Settings updated" };
+};
