5deec5ebf02f0ff83e31f47ab505399eb0c726c6
Sinisa <sinisha.stojanovic@gmail.com>
2025-10-03 11:43:49 +0200

    settings server action that change all data passed from SettingsPage form

--- actions/settings.ts
+++ actions/settings.ts
@@ -2,9 +2,12 @@
 
 import * as z from "zod";
 import { SettingsSchema } from "@/schemas";
-import { getUserById } from "@/data/user";
+import { getUserByEmail, getUserById } from "@/data/user";
 import { currentUser } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
+import bcrypt from "bcryptjs";
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
@@ -16,6 +19,44 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   if (user.id) dbUser = await getUserById(user.id);
 
   if (!dbUser) return { error: "Unauthorized" };
+
+  if (user.isOAuth) {
+    values.email = undefined;
+    values.password = undefined;
+    values.newPassword = undefined;
+    values.isTwoFactorEnabled = undefined;
+  }
+
+  if (values.email && values.email !== user.email) {
+    const existingUser = await getUserByEmail(values.email);
+
+    if (existingUser && existingUser.id !== user.id)
+      return { error: "Email already in use!" };
+
+    const verificationToken = await generateVerificationToken(values.email);
+
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token
+    );
+
+    return {
+      success:
+        "Verification email sent to new address. Please verify to update your email.",
+    };
+  }
+
+  if (values.password && values.newPassword && dbUser.password) {
+    const passwordMatch = await bcrypt.compare(
+      values.password,
+      dbUser.password
+    );
+    if (!passwordMatch) return { error: "Password incorrect!" };
+
+    const hashedPassword = await bcrypt.hash(values.newPassword, 12);
+    values.password = hashedPassword;
+    values.newPassword = undefined;
+  }
 
   await db.user.update({
     where: { id: dbUser.id },
