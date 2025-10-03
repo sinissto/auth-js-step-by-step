e5b415b51d6ac31d238bc2eaa9d1a2119ab65c1b
Sinisa <sinisha.stojanovic@gmail.com>
2025-10-03 10:19:41 +0200

    Session extended with  name, email and isOAuth, so it can be changed in settings page

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { db } from "@/lib/db";
 import { getUserById } from "@/data/user";
 import { UserRole } from "@/lib/generated/prisma";
 import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
+import { getAccountByUserId } from "@/data/account";
 
 export const {
   handlers: { GET, POST },
@@ -58,6 +59,11 @@ export const {
       const existingUser = await getUserById(token.sub);
       if (!existingUser) return token;
 
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
@@ -75,6 +81,12 @@ export const {
 
       if (session.user) {
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
+      }
+
+      if (session.user) {
+        session.user.name = token.name as string;
+        session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
       }
 
       return session;
